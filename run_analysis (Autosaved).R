4867758e589b8229d7410e57d6a8609d693101f8
jcwalmsley <jw.prof.cntrng@gmail.com>
2015-12-25 23:26:40 +0100

    getting data files
    project files

---
+++ run_analysis (Autosaved).R
@@ -0,0 +1,23 @@
+# Getting and Cleaning Data Course Project code:
+
+# Install R packages:
+packages <- c('base', 'curl', 'plyr', 'dplyr', 'data.table', 'httr', 'magrittr', 'reshape2', 'utils')
+sapply(packages, require, character.only = TRUE, quietly = TRUE)
+
+fileName# setwd:
+setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject")
+
+# Name a directory to save data to:
+fileName <- "get_Dataset.zip"
+
+# Create the local empty directory named above to store the Raw Data:
+if (!file.exists(fileName)) {
+# Assign a handle to the url data location & download the zip file containing the data
+fileUrl <- “https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip”
+download.file(fileUrl, fileName, method = "curl")
+}
+
+if (!file.exists("UCI HAR Dataset")) {
+    unzip(fileName)
+}
+ls()
--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,22 @@
 # Getting and Cleaning Data Course Project code:
 
-# Configure installation R packages:
-packages <- c(“base”, “curl”, “plyr”, “dplyr”, “data.table”, “http”, “magrittr”, “reshape2”, “tidyr”, “utils”)
+# Install R packages:
+packages <- c('base', 'curl', 'plyr', 'dplyr', 'data.table', 'httr', 'magrittr', 'reshape2', 'utils')
 sapply(packages, require, character.only = TRUE, quietly = TRUE)
 
-
-# setwd:
-setwd(“~/Desktop/Coursera_R/GetCleanDataCourseProject”)
+fileName# setwd:
+setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject")
 
 # Name a directory to save data to:
-smartphoneData <- "get_Dataset.zip"
+fileName <- "get_Dataset.zip"
 
 # Create the local empty directory named above to store the Raw Data:
-if (!file.exists(smartphoneData)) {
+if (!file.exists(fileName)) {
 # Assign a handle to the url data location & download the zip file containing the data
 fileUrl <- “https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip”
-download.file(fileUrl, smartphoneData, method = "curl")
+download.file(fileUrl, fileName, method = "curl")
 }
 
+if (!file.exists("UCI HAR Dataset")) {
+    unzip(fileName)
+}
