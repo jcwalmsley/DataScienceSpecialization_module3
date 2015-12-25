07d4b2427c6d03e9de5cef57f89e81cd6ac106a6
jcwalmsley <jw.prof.cntrng@gmail.com>
2015-12-25 17:31:28 +0100

    Update to run_analysis.R code build
    code build update

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,24 @@
-# Getting and Cleaning Data course project code:
+# Getting and Cleaning Data Course Project code:
+
+# Configure installation R packages:
+packages <- c(“base”, “curl”, “plyr”, “dplyr”, “data.table”, “http”, “magrittr”, “reshape2”, “tidyr”, “utils”)
+sapply(packages, require, character.only = TRUE, quietly = TRUE)
+
+
 # setwd:
-setwd("~/Desktop/Coursera_R/G&CD_CourseProject")
-# Download required files:
-if(!file.exists("./data")){dir.create("./data")}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
-unzip(zipfile="./data/Dataset.zip",exdir="./data")
+setwd(“~/Desktop/Coursera_R/GetCleanDataCourseProject”)
+
+
+
+# Create local empty directory if none exists to store the Raw Data:
+if (!file.exists(ProjData)) {
+	dir.create(ProData)
+}
+
+# Assign a handle to the url data location:
+url <- “https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip”
+projRawData <- “Dataset.zip”
+
+# Download Raw Data Zip file:
+download.file(url, file.path(ProData, projRawData))
+
