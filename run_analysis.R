01b48057cead84448f9d599274d321eb7c71580d
jcwalmsley <jw.prof.cntrng@gmail.com>
2015-12-27 18:01:10 +0100

    revisions to code

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,25 @@
 # Getting and Cleaning Data Course Project R code:
 
-# Install R packages:
+# Clean workspace:
+rm(list=ls())
 
+# Install R packages:
 packages <- c('base', 'curl', 'plyr', 'dplyr', 'data.table', 'httr', 'magrittr', 'reshape2', 'utils')
 sapply(packages, require, character.only = TRUE, quietly = TRUE)
 
-# setwd:
 
-setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject")
-
-# Create the local empty directory named above to store the Raw Data:
-
-if (!file.exists(UCI HAR Dataset)) {
-	create.dir(UCI HAR Dataset)
+# Create an empty directory for storing the zipped files:
+if (!file.exists("UCI HAR Dataset")) {
+	dir.create("UCI HAR Dataset")
 }
-	
-# Assign handle to the data location URL
 
-fileUrl <- “https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip”
+# Assign a handle to the data source URL
+motionDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip?accessType=DOWNLOAD"
 
 # Download the data
+download.file(motionDataUrl, destfile = "./Desktop/Coursera_R/UCI HAR Dataset", method = "curl")
+list.files("./UCI HAR Dataset")
 
-download.file(fileUrl, UHI HAR Dataset, method = "curl")
-
+# setwd:
+setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject/UCI HAR Dataset")
+ls()
