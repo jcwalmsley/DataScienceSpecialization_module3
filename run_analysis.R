e81a4e3497363d17259af3d775f59021a0595c28
jcwalmsley <jw.prof.cntrng@gmail.com>
2015-12-28 00:51:15 +0100

    Update run_analysis.R
    comoplete revision

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,76 @@
 # Getting and Cleaning Data Course Project R code:
-
+ 
 # Clean workspace:
-rm(list=ls())
+# rm(list=ls())
+ls()
+
+# setwd:
+setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject")
+ls()
 
 # Install R packages:
 packages <- c('base', 'curl', 'plyr', 'dplyr', 'data.table', 'httr', 'magrittr', 'reshape2', 'utils')
 sapply(packages, require, character.only = TRUE, quietly = TRUE)
 
+# Assign handle to source URL, download the data to directory called "data"
+if (!file.exists("phoneData")) {
+	fileUrl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+	zipfile="phoneData.zip"
+	download.file(fileUrl, destfile = zipfile, method = "curl")
+	dateDownloaded <- date()
+	dateDownloadedls
+	ls()
+	unzip("phoneData.zip", exdir = ".")
+	list.files("UCI HAR Dataset")
+ }
 
-# Create an empty directory for storing the zipped files:
-if (!file.exists("UCI HAR Dataset")) {
-	dir.create("UCI HAR Dataset")
+# filespath <- "C:/Users/jamescwalmsley/Desktop/Coursera_R/GetCleanDataCourseProject/UCIHAR"
+
+# READ
+subtest <- read.table("./UCIHAR/test/subject_test.txt")
+xtest <- read.table("./UCIHAR/test/X_test.txt")
+ytest <- read.table("./UCIHAR/test/y_test.txt")
+subtrain <- read.table("./UCIHAR/train/subject_train.txt")
+xtrain <- read.table("./UCIHAR/train/X_train.txt")
+ytrain <- read.table("./UCIHAR/train/y_train.txt")
+features <- read.table("./UCIHAR/features.txt")[,2]
+activities <- read.table("./UCIHAR/activity_labels.txt")[,2]
+ 
+
+# MERGE
+xmerg <- rbind(xtrain, xtest)
+ymerg <- rbind(ytrain, ytest)
+submerg <- rbind(subtrain, subtest)
+fullmerg <- cbind(submerg, ymerg, xmerg)
+
+# RENAME COLUMNS
+featureNames <- as.character(features[,2])
+newCols <- c("subject", "activity", featureNames)
+colnames(fullmerg) <- newCols
+
+# EXTRACT MEAN & STD
+meanCols <- grep("mean()", colnames(fullmerg))
+stdCols <- grep("std()", colnames(fullmerg))
+finalCols <- c(meancols, stdcols)
+finalCols <- sort(finalCols)
+finalCols
+df1 <- fullmerg[, c(1, 2, finalCols)]
+df1
+df2 <- df1 %>% select(-contains("meanFreq")) 
+dim(df2) # 10299r, 68c
+dim(fullmerg) # 10299r, 563c
+
+# REFINE RESULTS
+tidyData <- data_frame
+for (i in 1:30) {
+	subj <- subset(df2, subject==i)
+	for (j in 1:6) {
+		act <- subset(subj, activity==j)
+		intData <- as.vector(apply(act, 2, mean))
+		tidyData <- rbind(tidyData, intData)
+	}
 }
 
-# Assign a handle to the data source URL
-motionDataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip?accessType=DOWNLOAD"
-
-# Download the data
-download.file(motionDataUrl, destfile = "./Desktop/Coursera_R/UCI HAR Dataset", method = "curl")
-list.files("./UCI HAR Dataset")
-
-# setwd:
-setwd("~/Desktop/Coursera_R/GetCleanDataCourseProject/UCI HAR Dataset")
-ls()
+df2 <- aggregate(. ~subject + activity, fullmerg, mean)
+df2 <- df2[order(df2$subject, df2$activity),]
+write.table(df2, file = "tidyData.txt", row.names=FALSE)
