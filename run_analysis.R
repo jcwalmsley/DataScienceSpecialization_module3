b99cef894546aaecb4d9f8386123dd1070a87d88
jcwalmsley <jw.prof.cntrng@gmail.com>
2016-01-14 10:14:25 +0100

    clarifications

--- run_analysis.R
+++ run_analysis.R
@@ -60,11 +60,13 @@ activities <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE
 ls()
 
 # ==================================
-# combine by columns; the -train sets- of (subjects or Z) with (activities or Y)
+# combine by columns; the -train sets- of (subjects or Z)
+# with (activities or Y)
 subAct1 <- cbind(trainZ, trainY)
 subAct1
 # =================================
-# combine by columns; the -test sets- of (subject or Z) with (activity or Y)
+# combine by columns; the -test sets- of (subject or Z)
+# with (activity or Y)
 subAct2 <- cbind(testZ, testY)
 subAct2
 # =================================
@@ -78,12 +80,14 @@ colnames(subAct) <- c("ID", "V1")
 head(subAct)
 
 #===================================
-# combine by rows; the "X" train and test values and assign to "observ" = observations
+# combine by rows; train and test datasets of "X" and assign
+# to "observ" = observations
 observ <- rbind(trainX, testX)
 head(observ)
 
 #===================================
-# join "subAct" with activites by variable "V1" and assign to "pA" = population & activities
+# join "subAct" with the activity descriptions = "activities"
+# by variable "V1" and assign to "pA" = population & activities
 pA <- join(subAct, activities)
 head(pA)
 
@@ -92,48 +96,63 @@ head(pA)
 # rename columns of (pA):
 names(pA) <- c("subjectID", "activityID", "activityName")
 head(pA)
+
+# ==================================
+# set values of "activityName to lower case
 pA$activityName <- tolower(pA$activityName)
 pA
 
 #===================================
-# merge "subAct" with "observ" = the descriptions of what the observations
-# are of with the coressponding columns of vlaues
+# merge "subAct" with "observ" by column bind resulting
+# in full raw data set and assign to "allData"
 allData <- cbind(pA, observ)
 head(allData)[1:12]
 
 #===================================
-# from (allData) remove column 2 and assign "dataT1" = subjectActivites
+# remove column 2 "activityID" from (allData) assign results to
+# "dataT1" = (subject, activites and values)
 dataT1 <- (allData)[-2]
 head(dataT1)[1:30]
 
 #===================================
-# make lowercase the column names of the observatoin values
+# set "features" = the observation values column names to lowercase
 varNames <- tolower(features[,2])
 varNames
+
+#===================================
+# concatenate the column names vector of "pA" and "observ"
+# for the new data set "allData" and assign to "allColumnNames"
 allColumnNames <- c("subjectID", "activityName", varNames)
 head(allColumnNames)
 length(allColumnNames)
+
 #===================================
-# set the column names for the subjects, activities and observation values
+# set the column names for "dataT1" = subjects, activities and
+# observation values
 names(dataT1) <- allColumnNames
 head(dataT1)[1:8]
 tail(dataT1)
 
 #===================================
-# select (subjectID, activityName, and only remaining colums containing (mean or std) and check for duplicates in the new subset
+# select from "dataT1" (subjectID, activityName, plus only colums containing
+# (mean or std) and assign results to "dataT2"
 index <- grepl("(ID$)|(Name$)|(mean\\(\\))|(std\\(\\))", allColumnNames)
 index
 dataT2 <- dataT1[,index==TRUE]
 dim(dataT2)
 head(dataT2)[1:15]
+
 #===================================
-# remove duplicate columns if any exist
+# if any exist, remove duplicate columns and assign
+# results to "finalData"
 finalData <- dataT2[, !duplicated(colnames(dataT2))]
 head(finalData)
 dim(finalData)
 
 #===================================
-# group by subject and activity, then summarize by mean
+# group "finalData" by subject and activity assign the
+# results to "groupedData" then summarize by mean and
+# assign new results to "tidyData"
 library(dplyr)
 groupedData <- group_by(finalData, subjectID, activityName)
 tidyData <- summarise_each(groupedData, funs(mean))
@@ -141,7 +160,7 @@ dim(tidyData)
 summary(tidyData)
 tidyData
 #===================================
-# write output to a file called tidyData.txt
+# take "tidyData" and write it as output to a file called "tidyData.txt"
 write.table(tidyData, "tidyData.txt", row.names = FALSE, quote = FALSE, sep = "\t")
 date()
 # [1] "Tue Jan 12 23:17:43 2016"
@@ -150,7 +169,6 @@ date()
 
 =======
 >>>>>>> Stashed changes
-
 
 
 
